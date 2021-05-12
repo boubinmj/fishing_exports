ea338ade6e0f8748c3cb0f4c033b545c1f887316
boubinmj <boubinmj@miamioh.edu>
2021-05-12 11:25:24 -0400

    fixed colname bug

--- global.R
+++ global.R
@@ -11,6 +11,15 @@ by_years = read.csv('DATA/by_years.csv', header = TRUE)
 by_species = read.csv('DATA/by_species.csv', header = TRUE)
 by_Norway = read.csv('DATA/by_norway.csv', header = TRUE)
 
-countries <- distinct(by_years %>% select(., Country))
-species <- distinct(by_years %>% select(., Species))
-years_a <- distinct(by_species %>% select(., Year))
+head(by_years)
+head(by_species)
+head(by_Norway)
+
+species <- colnames(by_Norway)[-1][-1]
+years_a <- colnames(by_years)[-1][-1]
+countries <- by_species["Country"]
+
+
+# countries <- distinct(by_years %>% select(., Country))
+# species <- distinct(by_years %>% select(., Species))
+# years_a <- distinct(by_species %>% select(., Year))
